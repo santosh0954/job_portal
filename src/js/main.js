b5faa6a523b983e72d50661568d9a59d2f528463
santosh jha <santosh0954jha@gmail.com>
2025-07-21 23:05:56 +0530

    admin internal pages added for category, post and event table list of that page

--- src/js/main.js
+++ src/js/main.js
@@ -51,4 +51,41 @@ $(function () {
   $(".menu-toggle-btn").on("click", function () {
     $(".navbar-nav").slideToggle();
   });
+
+  // adding field on click
+  $(".add-field").on("click", function () {
+    let count = $(this).parent().find(">div").length;
+    $(this).parent().find(">div").addClass("relative");
+    let html = $(this).parent().find(">div").first().clone();
+    $(html).append(
+      `<button class="btn text-xs p-1 aspect-square flex items-center content-center border-red-300 bg-red-200 text-red-600 hover:border-red-600 opacity-70 hover:opacity-100 shadow-none hover:text-white hover:bg-red-600 absolute top-0 right-0" title="Remove field"> <i class="fa fa-close"></i> </button>`
+    );
+    $(html)
+      .find(".btn")
+      .on("click", function () {
+        $(this).parent().remove();
+      });
+    $(html)
+      .find("label")
+      .each(function () {
+        $(this)
+          .attr("for", $(this).attr("for") + (count + 1))
+          .append(` ${count + 1} `);
+      });
+    $(html)
+      .find("input, textarea")
+      .each(function () {
+        $(this).attr("id", $(this).attr("id") + (count + 1));
+      });
+    $(this).parent().find(">div").last().after(html);
+  });
+  // category on change to visible admit, result publish
+  $("#category").on("change", function () {
+    let selected = $(this).val();
+    if (selected === "1") {
+      $(".publish-sec").slideDown();
+    } else {
+      $(".publish-sec").slideUp();
+    }
+  });
 });
